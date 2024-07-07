5ea0fa430620d72cdfe46aa45d50ea6429a7fecc
AmanKalabay <149198402+Amansdfg@users.noreply.github.com>
2024-07-07 13:37:05 +0500

    commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,27 +1,30 @@
-import {useState} from "react";
+import {useInput} from "../hooks/useInput.js";
+import {isEmail,isNotEmpty,hasMinLength,isEqualsToOtherValue} from "../util/validation.js"
 export default function Login() {
-    const[enteredValues,setEnteredValues]=useState({
-        email:'',
-        password:''
-    })
-    const emailIsInvalid= enteredValues.email!=='' && !enteredValues.email.includes("@");
-    function handleInputChange(identifier,value){
-        setEnteredValues((prev)=>{
-            return{
-                ...prev,
-                [identifier]:value
-            }
-        })
-    }
+    const {
+      value:emailValue,
+      handleInputChange:handleEmailChange,
+      handleInputBlur : handleEmailBlur,
+      hasError:emailHasError,
+    } = useInput('',(value)=>isEmail(value) && isNotEmpty(value));
+    const {
+      value:passwordValue,
+      handleInputChange:handlePasswordChange,
+      handleInputBlur:handlePasswordBlur,
+      hasError:passwordHasError,
+    }=useInput('',(value)=>isNotEmpty(value) && hasMinLength(value,6));
+
     function handleSubmit(event){
-        event.preventDefault();
-        console.log("Submited");
-        console.log(enteredValues.email+" "+enteredValues.password);
-    }
+      event.preventDefault();
+      if(passwordHasError || emailHasError){
+        return;
+      }
+      console.log("Submited");
+      console.log(emailValue+" "+passwordValue);
+  }
     return (
       <form onSubmit={handleSubmit}>
         <h2>Login</h2>
-  
         <div className="control-row">
           <div className="control no-margin">
             <label htmlFor="email">Email</label>
@@ -29,10 +32,11 @@ export default function Login() {
                 id="email" 
                 type="email" 
                 name="email" 
-                onChange={(event)=>handleInputChange("email",event.target.value)} 
-                value={enteredValues.email}
+                onBlur={handleEmailBlur}
+                onChange={handleEmailChange} 
+                value={emailValue}
                 />
-            { emailIsInvalid && <div className="control-error">Invalid</div> }
+                {emailHasError && <div className="control-error">Invalid Email</div> }
           </div>
   
           <div className="control no-margin">
@@ -41,9 +45,11 @@ export default function Login() {
                 id="password" 
                 type="password" 
                 name="password" 
-                onChange={(event)=>handleInputChange("password",event.target.value)}
-                value={enteredValues.password}
+                onBlur={handlePasswordBlur}
+                onChange={handlePasswordChange}
+                value={passwordValue}
                 />
+              { passwordHasError && <div className="control-error">Invalid Password</div> }
           </div>
         </div>
   
